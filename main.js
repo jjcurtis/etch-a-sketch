47a8a9c895a15ea961ef475a946b24bb25fb2a1a
Jonathan <jonathan.j.curtis@outlook.com>
2023-01-19 11:30:07 -0600

    Add column divs to HTML and simplify JS

--- main.js
+++ main.js
@@ -2,12 +2,11 @@ let divCount = 16;
 
 const body = document.querySelector('.body');
 const column = document.querySelectorAll('.column');
-let div = document.querySelectorAll('div');
 
 for (let i = 0; i < divCount; i++) {
-    let createDiv = document.createElement('div');
-    createDiv.className = 'column';    
-    body.appendChild(createDiv);
+    column.forEach(column => {
+        let createDiv = document.createElement('div');
+        column.appendChild(createDiv);
+    })
 }
-
 
