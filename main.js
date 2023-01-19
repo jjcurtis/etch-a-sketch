170779d39e63c11b333c87fe42344dff9188a953
Jonathan <jonathan.j.curtis@outlook.com>
2023-01-19 12:27:53 -0600

    Create mouseover styling of squares

--- main.js
+++ main.js
@@ -1,12 +1,19 @@
 let divCount = 16;
 
 const body = document.querySelector('.body');
-const column = document.querySelectorAll('.column');
+const column = document.querySelectorAll('div.column');
+let divs = document.querySelectorAll('div');
 
 for (let i = 0; i < divCount; i++) {
     column.forEach(column => {
         let createDiv = document.createElement('div');
+        createDiv.className = 'square'
         column.appendChild(createDiv);
     })
+    let squares = document.querySelectorAll('div.square');
+    squares.forEach(square => {
+        square.addEventListener('mouseover', () => {
+            square.style.backgroundColor = 'red';
+        })
+    })
 }
-
