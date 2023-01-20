e722010767568db6471b3cfd885f1a49b9dd688f
Jonathan <jonathan.j.curtis@outlook.com>
2023-01-19 22:06:06 -0600

    Fix rows to display equal number of divs

--- main.js
+++ main.js
@@ -8,19 +8,22 @@ for (let i = 0; i < divCount; i++) {
         createDiv.className = 'column';
         body.appendChild(createDiv);
     })
-    
-    const column = document.querySelectorAll('.column');
+}
 
+const column = document.querySelectorAll('.column');
+
+for (let i = 0; i < divCount; i++) {
     column.forEach(column => {
         let createDiv = document.createElement('div');
         createDiv.className = 'square';
         column.appendChild(createDiv);
     })
-
-    let squares = document.querySelectorAll('.square');
+    
+}    
+    
+let squares = document.querySelectorAll('.square');
     squares.forEach(square => {
         square.addEventListener('mouseover', () => {
             square.style.backgroundColor = 'red';
         })
     })
-}
