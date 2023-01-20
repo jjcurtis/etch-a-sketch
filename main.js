7394cff893ff62e1e0202d09baa9f5826d3e6fd9
Jonathan <jonathan.j.curtis@outlook.com>
2023-01-20 14:08:46 -0600

    Add event listener to adjust grid size

--- main.js
+++ main.js
@@ -1,44 +1,43 @@
-let divCount = 16;
+let gridSize = document.querySelector('button');
 
-const body = document.querySelectorAll('.body');
+gridSize.addEventListener('click', () => {
+    const body = document.querySelectorAll('.body');
+    let number = parseInt(prompt('Enter the number of squares per side'));
+    let divCount;
+    divCount = number;
+    if (divCount > 100) {
+        alert('Number exceeds threshold (100)');
+    }
 
-for (let i = 0; i < divCount; i++) {
-    body.forEach(body => {
-        let createDiv = document.createElement('div');
-        createDiv.className = 'column';
-        body.appendChild(createDiv);
-    })
-}
-
-const column = document.querySelectorAll('.column');
-
-for (let i = 0; i < divCount; i++) {
-    column.forEach(column => {
-        let createDiv = document.createElement('div');
-        createDiv.className = 'square';
-        column.appendChild(createDiv);
-    })
+    for (let i = 0; i < divCount; i++) {
+        body.forEach(body => {
+            let createDiv = document.createElement('div');
+            createDiv.className = 'column';
+            body.appendChild(createDiv);
+        })
+    }
     
-}    
+    const column = document.querySelectorAll('.column');
     
-let squares = document.querySelectorAll('.square');
-squares.forEach(square => {
-square.addEventListener('mouseover', () => {
-        square.style.backgroundColor = 'red';
+    for (let i = 0; i < divCount; i++) {
+        column.forEach(column => {
+            let createDiv = document.createElement('div');
+            createDiv.className = 'square';
+            column.appendChild(createDiv);
+        })
+        
+    }    
+    
+    let squares = document.querySelectorAll('.square');
+    squares.forEach(square => {
+    square.addEventListener('mouseover', () => {
+        square.style.backgroundColor = 'purple';
+        })
     })
+
+    let divs = document.querySelectorAll('div');
+    divs.forEach(div => div.style.border = '2px solid black');
+
+    gridSize.remove();
 })
 
-// let gridSize = document.querySelector('.button');
-// gridSize.addEventListener('click', () => {
-//     let number = prompt('Enter the number of squares per side');
-//     if (number < 100) {
-//         column.forEach(column => {
-//             column.remove();
-//             column.appendChild('div')
-//         })
-//         divCount = number;
-//         return divCount;
-//     } else {
-
-//     }
-// })
